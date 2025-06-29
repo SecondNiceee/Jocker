94502c722591dfc15f746ea3aaa416b575711648
SecondNiceee <col1596321@gmail.com>
2025-06-29 14:55:27 +0300

    redeploy

--- src/http/index.js
+++ src/http/index.js
@@ -4,7 +4,7 @@ import parseTelegramData from "../functions/parseTelegramData";
 const $api = axios.create({
   baseURL: process.env.REACT_APP_HOST,
   headers: {
-    "x-init-data": parseTelegramData(window.Telegram.WebApp.initData),
+    "x-init-data": window.Telegram.WebApp.initData,
   },
 });
 
