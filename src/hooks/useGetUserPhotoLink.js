bc903afd942990bffb5d5377c19ab47dcf76c0d6
SecondNiceee <col1596321@gmail.com>
2025-07-03 16:45:41 +0300

    fix photos

--- src/hooks/useGetUserPhotoLink.js
+++ src/hooks/useGetUserPhotoLink.js
@@ -5,11 +5,11 @@ const useGetUserPhotoLink = ({anotherUserInfo = null}) => {
     
     const me = useSelector((state) => state.telegramUserInfo);
     const link = useMemo( () => {
-        if (!me.id || !anotherUserInfo){
+        if (!me.id && !anotherUserInfo){
             return "/images/icons/non-user-icon.png"   
         }
         if (anotherUserInfo){
-            if (!anotherUserInfo.photo || anotherUserInfo.photo?.length){
+            if (!anotherUserInfo.photo || !anotherUserInfo.photo?.length){
                 return "/images/icons/non-user-icon.png"
             }
             return anotherUserInfo.photo;
